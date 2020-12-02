b7b83579b14a7c725a3c569ae812a4a7a38c98ce
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-12-02 23:21:36 +0530

    ed

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -4,14 +4,14 @@ const Selection = (props) => {
   let [nextBackground, selectNextBackground] = React.useState({
     background: ""
   });
-  const handle = (background) => {
-    selectNextBackground(background);
-  };
+  // const handle = (background) => {
+  //   selectNextBackground(background);
+  // };
   return (
     <div
       className="fix-box"
       style={nextBackground}
-      onClick={() => applyColor(handle)}
+      onClick={() => applyColor(selectNextBackground)}
     >
       <h2 class="subheading">Selection</h2>
     </div>
